b13d13e1ab7769ed3873df0db57d812f46b7f569
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-08 06:46:48 -0600

    Create App.jsx and App.css in directory app; global styles in index.css

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+   render() {
+      return (
+         <div className='app__container'>
+            <h1>React-Routing-Example</h1>
+         </div>
+      );
+   };
+};
