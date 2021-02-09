292dde92f9f18b2779e5384754fa8cee4e190bf3
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-09 08:05:58 -0600

    Add formik package and skelton of ContactPage.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import './Header.css';
 import HomePage from './../../pages/home-page/HomePage';
 import GithubUser from './../github-user/GithubUser';
 import NotFoundPage from './../../pages/not-found-page/NotFoundPage';
+import ContactPage from './../../pages/contact/ContactPage';
 
 export default class Header extends Component {
    render() {
@@ -12,6 +13,7 @@ export default class Header extends Component {
             <Switch>
                <Route exact path='/' component={HomePage} />
                <Route path='/github' component={GithubUser} />
+               <Route path='/contact' component={ContactPage} />
                <Route path='/*' component={NotFoundPage} />
             </Switch>
          </BrowserRouter>
--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -4,8 +4,29 @@ import './ContactPage.css';
 export default class ContactPage extends Component {
    render() {
       return (
-         <div>
-            
+         <div className='contactPage'>
+            <h2 className='contactPage__heading'>Contact Us</h2>
+            <Form>
+               <div>
+                  <Field
+                     type='email'
+                     name='email'
+                     placeholder='email address'
+                  />
+                  <ErrorMessage name='email' component='span' />
+               </div>
+               <div>
+                  <Field
+                     type='password'
+                     name='password'
+                     placeholder='password'
+                  />
+                  <ErrorMessage name='password' component='span' />
+               </div>
+               <button type='submit' disabled={isSubmitting}>
+                  Submit
+                     </button>
+            </Form>
          </div>
       );
    };
