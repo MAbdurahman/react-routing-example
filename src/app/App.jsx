399390e4dd35a01267b1beccc8e95fb7c33ac01c
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-09 04:26:58 -0600

    Complete NotFoundPage and styles (not responsive)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import NotFoundPage from '../pages/not-found-page/NotFoundPage';
 import './App.css';
 
 export default class App extends Component {
    render() {
       return (
          <div className='app__container'>
-            <h1>React-Routing-Example</h1>
+            <NotFoundPage />
          </div>
       );
    };
---
+++ src/pages/not-found-page/NotFoundPage.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import './NotFoundPage.css';
+import HomePage from './../home-page/HomePage';
+
+export default class NotFoundPage extends Component {
+   render() {
+      return (
+         <div className='notFoundPage'>
+            <h1 className='notFoundPage__Title'>404</h1>
+            <h2 className='notFoundPage__subTitle'>Oops!&nbsp; That page can’t be found.</h2>
+            <h4 className='notFoundPage__description'>
+               It looks like nothing was found at this location.
+               Maybe try one of the links in the menu or press the back to home to button.
+            </h4>
+            <Button className='notFoundPage__button'><a href='#'>Back to Home</a></Button>
+         </div>
+      );
+   };
+};
