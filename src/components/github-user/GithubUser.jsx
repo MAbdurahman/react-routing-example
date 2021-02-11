f959dba2b8aa12722b178898ef7e89ea807baa26
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-11 16:31:43 -0600

    Temporary Hotfix

--- src/components/github-user/GithubUser.jsx
+++ src/components/github-user/GithubUser.jsx
@@ -55,8 +55,8 @@ export default class GithubUser extends Component {
                   className='mr-3'
                   src={user.avatar_url}
                   alt='Generic placeholder'
-                  width={81}
-                  height={81}
+                  width={64}
+                  height={64}
                />
             </a>
             <Media.Body className='githubMediaBody'>
@@ -79,8 +79,8 @@ export default class GithubUser extends Component {
                   <Button className='githubButton' type='submit'>Search</Button>
                </FormGroup>
             </Form>
+            <h3 className='githubSubheading'>GitHub Users Results</h3>
             <section className='githubSection'>
-               <h3 className='githubSubheading'>GitHub Users Results</h3>
                {this.state.isLoading &&
                   <ReactLoading
                      className='githubLoader'
